c8f3ce312e514b9ecdfb475cd1d5c1460efaeaf7
Sujay Solomon <solsu01@ca.com>
2018-11-29 21:56:36 -0500

    remove scripts

--- OtherScripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#deploy code
-bright fmp copy ds CA90SMVS.MOTM.CCS.DEMO.D1.LOADLIB SOLSU01.COBOL.LOAD --members COBLSAMP
--- OtherScripts/pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#download code
-bright endevor list ele -i WEBSMFNE --env dev --sys ccs --sub demo
-
-bright endevor retrieve element COBLSAMP --env dev --sys ccs --sub demo --typ cobpgm --tf samp.cbl --i WEBSMFNE --sn 1 --ccid share --com "SHARE demo"
